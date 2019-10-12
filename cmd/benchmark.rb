6619c82c5a55968f13bc5db3eec22deab8ac72f6
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2019-10-11 21:34:37 -0400

    Add ripgrep benchmarks

--- cmd/benchmark.rb
+++ cmd/benchmark.rb
@@ -2,13 +2,14 @@ require 'benchmark'
 require 'csv'
 require 'pry'
 
-num_pats = %i(10 100 1000 10000 100000)
-num_text_lines = %i(10 100 1000 10000 50000)
+num_pats = %i(100000 10000 1000 100 10)
+num_text_lines = %i(10000 1000 100 10)
 
 class Output
-  def initialize(biblio_data, grep_data, lines, pats)
+  def initialize(biblio_data, grep_data, ripgrep_data, lines, pats)
     @biblio_data = biblio_data
     @grep_data = grep_data
+    @ripgrep_data = ripgrep_data
     @lines = lines
     @pats = pats
   end
@@ -28,6 +29,10 @@ class Output
   def grep_a
     [ sprintf("%0.05f", @grep_data.real) ]
   end
+
+  def ripgrep_a
+    [ sprintf("%0.05f", @ripgrep_data.real) ]
+  end
 end
 
 outputs = []
@@ -35,15 +40,16 @@ outputs = []
 Benchmark.bm do |x|
   num_text_lines.each do |lines|
     num_pats.each do |pats|
-      grep_output = x.report("grep -F") { `grep -F -f pats-#{pats}.txt text-#{lines}.txt` }
+      grep_output = x.report("grep -F") { `grep -F -f pats-#{pats}.txt text-#{lines}.txt -c` }
+      ripgrep_output = x.report("ripgrep -F") { `rg -F -f pats-#{pats}.txt text-#{lines}.txt -c` }
       biblio_output = x.report("biblio") { `./cmd -patternsFile pats-#{pats}.txt -file text-#{lines}.txt` }
 
-      outputs << Output.new(biblio_output, grep_output, lines, pats)
+      outputs << Output.new(biblio_output, grep_output, ripgrep_output, lines, pats)
     end
   end
 end
 
-headers = ['patterns', 'lines', '', 'biblio', 'fgrep', ]
+headers = ['patterns', 'lines', '', 'biblio', 'grep -F', 'rg -F']
 
 CSV.open(
   'benchmarks.csv',
@@ -52,6 +58,6 @@ CSV.open(
   headers: headers
 ) do |writer|
   outputs.each do |output|
-    writer << [output.pats, output.lines, '', *output.biblio_a, *output.grep_a]
+    writer << [output.pats, output.lines, '', *output.biblio_a, *output.grep_a, *output.ripgrep_a]
   end
 end
